2cc453030341991cc7734e4364f2a2ffa6209b1b
Grant Fleming <grantgfleming@googlemail.com>
2020-04-18 17:21:42 +0100

    Add error handling to PostService

--- src/app/forum/services/post.service.ts
+++ src/app/forum/services/post.service.ts
@@ -1,8 +1,9 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
+import {Observable, of} from 'rxjs';
 import {Post} from '../models/post';
 import {environment} from '../../../environments/environment';
+import {catchError} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -16,11 +17,32 @@ export class PostService {
   }
 
   getPosts(): Observable<Post[]> {
-    return this.http.get<Post[]>(`${this.baseURL}${this.postsEndpoint}`);
+    return this.http.get<Post[]>(`${this.baseURL}${this.postsEndpoint}`)
+      .pipe(
+        catchError(this.handleError<Post[]>([]))
+      );
   }
 
   addPost(post: Post): Observable<Post> {
-    return this.http.post<Post>(`${this.baseURL}${this.postsEndpoint}`, post);
+    if (post.id) {
+      throw new Error('Post cannot already contain an id as this can be overridden by the server.');
+    }
+
+    return this.http.post<Post>(`${this.baseURL}${this.postsEndpoint}`, post)
+      .pipe(
+        catchError(this.handleError<Post>(post))
+      );
   }
 
+  /**
+   * Handle an error thrown by an observable and instead return an Observable
+   * of some default value.
+   *
+   * TODO - introduce some logging service and log the error
+   *
+   * @param defaultValue to return
+   */
+  private handleError<T>(defaultValue?: T) {
+    return (error: any): Observable<T> => of(defaultValue as T);
+  }
 }
