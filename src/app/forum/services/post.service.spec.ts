d95f49d09eadc2db3584e72b4b88c0e084f35147
Grant Fleming <grantgfleming@googlemail.com>
2020-04-20 15:13:40 +0100

    Refactor PostService tests

--- src/app/forum/services/post.service.spec.ts
+++ src/app/forum/services/post.service.spec.ts
@@ -1,43 +1,61 @@
 import {PostService} from './post.service';
-import {Observable} from 'rxjs';
 import {Post} from '../models/post';
+import {HttpErrorResponse} from '@angular/common/http';
+import {fakeAsync} from '@angular/core/testing';
+import {asyncError, genericError} from '../../test_utils/test_async_utils';
 
 describe('PostService', () => {
   let service: PostService;
   let mockHttpClient;
+  const response404 = new HttpErrorResponse({error: 'test 404 error', status: 404, statusText: 'Not Found'});
+
 
   beforeEach(() => {
     mockHttpClient = jasmine.createSpyObj(['get', 'post']);
-    mockHttpClient.get.and.returnValue(
-      new Observable(
-        subscriber =>
-          subscriber.next([{id: 1, title: 'A post', body: 'Some text to make up the body of a post'}]
-          )));
-
     service = new PostService(mockHttpClient);
   });
+
 
   it('should create', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should return empty list on error in getPosts', () => {
-    mockHttpClient.get.and.returnValue(
-      new Observable(subscriber => subscriber.error('any'))
-    );
-    service.getPosts().subscribe(value => expect(value).toEqual([]));
-  });
 
-  it('should return the same post back on error in addPost', () => {
+  it('#getPosts should return empty list on error', fakeAsync(() => {
+    mockHttpClient.get.and.returnValue(
+      asyncError(genericError)
+    );
+    service.getPosts().subscribe(
+      value => expect(value).toEqual([]),
+      () => fail(' expected empty list on error, not error'));
+  }));
+
+
+  it('#getPosts should return empty list on 404', fakeAsync(() => {
+    mockHttpClient.get.and.returnValue(
+      asyncError(response404)
+    );
+
+    service.getPosts().subscribe(
+      posts => expect(posts).toEqual([]),
+      () => fail('expected empty list on 404, not error'));
+  }));
+
+
+  it('#addPost should return the same post back on error', fakeAsync(() => {
     // The lack of an id in the returned post is an indication that an error occurred
+    // this includes a 404 via HttpErrorResponse
     mockHttpClient.post.and.returnValue(
-      new Observable(subscriber => subscriber.error('any'))
+      asyncError(genericError)
     );
     const post = new Post();
     post.title = 'a title';
     post.body = 'some body text';
-    service.addPost(post).subscribe(value => expect(value).toEqual(post));
-  });
+    service.addPost(post).subscribe(
+      value => expect(value).toEqual(post),
+      () => fail('expected a Post object, not an error'));
+  }));
+
 
   it('should not try to add a post that already has an id', () => {
     const post = new Post();
