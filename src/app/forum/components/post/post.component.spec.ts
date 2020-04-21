27db0703c2336390a80e5cc2833caa203af399b6
Grant Fleming <grantgfleming@googlemail.com>
2020-04-21 11:19:04 +0100

    Add tests for PostComponent

--- src/app/forum/components/post/post.component.spec.ts
+++ src/app/forum/components/post/post.component.spec.ts
@@ -1,10 +1,14 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {PostComponent} from './post.component';
+import {Post} from '../../models/post';
+import {By} from '@angular/platform-browser';
+import {DebugElement} from '@angular/core';
 
 describe('PostComponent', () => {
   let component: PostComponent;
   let fixture: ComponentFixture<PostComponent>;
+  let expectedPost: Post;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,10 +20,24 @@ describe('PostComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PostComponent);
     component = fixture.componentInstance;
+    expectedPost = {id: 1, title: 'A post title', body: 'A post body'};
+    component.post = expectedPost;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display the title', () => {
+    const titleDe: DebugElement = fixture.debugElement.query(By.css('.title'));
+    const titleNe = titleDe.nativeElement;
+    expect(titleNe.textContent).toBe(expectedPost.title);
+  });
+
+  it('should display the body', () => {
+    const bodyDe: DebugElement = fixture.debugElement.query(By.css('.body'));
+    const bodyNe = bodyDe.nativeElement;
+    expect(bodyNe.textContent).toBe(expectedPost.body);
   });
 });
