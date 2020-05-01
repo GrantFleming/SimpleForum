888d8d367756bd9ce951b51f0bd25792cde5e0a3
Grant Fleming <grantgfleming@googlemail.com>
2020-05-01 17:46:58 +0100

    feature: implement caching in the PostService
    PostService now caches responses, It will first respond from the cache initially if it has a cache for the request, then provide newer values if it receives some from the server later.

--- src/app/forum/services/forum.service.ts
+++ src/app/forum/services/forum.service.ts
@@ -59,7 +59,7 @@ export class ForumService {
     const values = Array.from(this.cache.values());
 
     // returns the cached forums only if there are forums in the cache
-    const $cachedForums = of(values.map(val => val[0]))
+    const cachedForums$ = of(values.map(val => val[0]))
       .pipe(
         filter(forums => forums.length !== 0));
 
@@ -71,12 +71,12 @@ export class ForumService {
       headers = headers.set('If-Modified-Since', oldestDate.toUTCString());
     }
 
-    const $serverResponse = this.http.get(
+    const serverResponse$ = this.http.get(
       `${environment.backendHost}/forums`,
       {headers, observe: 'response'});
 
     // If we are supplied newer forums, update the cache then return them
-    const $updatedForums = $serverResponse
+    const updatedForums$ = serverResponse$
       .pipe(
         filter(response => response.status === 200),
         tap(response => {
@@ -93,7 +93,7 @@ export class ForumService {
     plus the server not being able to provide us with a successful response, return
     an empty forum list.
      */
-    return concat($cachedForums, $updatedForums).pipe(defaultIfEmpty([]));
+    return concat(cachedForums$, updatedForums$).pipe(defaultIfEmpty([]));
   }
 
   /**
@@ -121,7 +121,7 @@ export class ForumService {
     const cachedValue = this.cache.get(id);
 
     // returns the cached forum if it exists
-    const $cachedForum = cachedValue ? of(cachedValue[0]) : EMPTY;
+    const cachedForum$ = cachedValue ? of(cachedValue[0]) : EMPTY;
 
     // if the cache contains a value for the given id, use the date to get the 'If-Modified-Since' header value
     let headers = new HttpHeaders();
@@ -130,12 +130,12 @@ export class ForumService {
       headers = headers.set('If-Modified-Since', date.toUTCString());
     }
 
-    const $serverResponse = this.http.get(
+    const serverResponse$ = this.http.get(
       `${environment.backendHost}/forums/${id}`,
       {headers, observe: 'response'});
 
     // If we are supplied a newer forum, update the cache then return it
-    const $updatedForum = $serverResponse
+    const updatedForum$ = serverResponse$
       .pipe(
         filter(response => response.status === 200),
         tap(response => {
@@ -151,7 +151,7 @@ export class ForumService {
     plus the server not being able to provide us with a successful response, return
     an empty forum list.
      */
-    return concat($cachedForum, $updatedForum).pipe(defaultIfEmpty(null));
+    return concat(cachedForum$, updatedForum$).pipe(defaultIfEmpty(null));
   }
 
   /**
--- src/app/forum/services/post.service.ts
+++ src/app/forum/services/post.service.ts
@@ -1,11 +1,14 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {Observable, of} from 'rxjs';
+import {concat, EMPTY, Observable, of} from 'rxjs';
 import {Post} from '../models/post';
 import {environment} from '../../../environments/environment';
-import {catchError} from 'rxjs/operators';
+import {catchError, defaultIfEmpty, filter, map, pluck, tap} from 'rxjs/operators';
 
-const cudOptions = {headers: new HttpHeaders({'Content-Type': 'application/json'})};
+const cudOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'}),
+  observe: 'response' as 'response'
+};
 
 @Injectable({
   providedIn: 'root'
@@ -14,42 +17,101 @@ export class PostService {
 
   private baseURL: string = environment.backendHost;
   private postsEndpoint = '/posts';
+  private cache: Map<number, [Post, Date]> = new Map();
 
   constructor(private http: HttpClient) {
   }
 
+  /**
+   *  Returns a list of posts, maybe from the cache maybe from the server,
+   *  maybe from the cache then later from the server. The returned Observable
+   *  can make a maximum of two emissions.
+   *
+   *  There are four possible cases this method handles:
+   *
+   *              | server responds 200   | server responds non-200
+   *              | with new data         | and provides no data
+   *  ------------|---------------------------------------------------
+   *  cache empty | Cache updated         | [] returned on server
+   *              | New values returned   | response
+   *  ------------|---------------------------------------------------
+   *  cache active| Cache value returned  | Cache value returned
+   *              | initially ...         |
+   *              | Server value returned | Observable completes on
+   *              | when it arrives ...   | server response
+   *
+   * @param forumId the id of the forum for which posts are required
+   */
   getPosts(forumId: number): Observable<Post[]> {
-    return this.http.get<Post[]>(`${this.baseURL}${this.postsEndpoint}?forumId=${forumId}`)
-      .pipe(
-        catchError(this.handleError<Post[]>([]))
-      );
-  }
+    const relevantValues = Array.from(this.cache.values())
+      .filter(val => val[0].forumId === forumId);
+    const relevantPosts = relevantValues.map(val => val[0]);
+    const cachedPosts$ = of(relevantPosts).pipe(
+      filter(posts => posts.length !== 0)
+    );
 
-  addPost(post: Post): Observable<Post> {
-    /*
-      TODO - alter to conform with how a POST request would actually be responded
-
-      In reality a post request responds 201 and the location of the newly created resource
-     */
-    if (post.id) {
-      throw new Error('Post cannot already contain as this is assigned by the server.');
+    let headers = new HttpHeaders();
+    if (relevantValues.length > 0) {
+      const dates = relevantValues.map(pair => pair[1].getTime());
+      const oldestDate = new Date(Math.min(...dates));
+      headers = headers.set('If-Modified-Since', oldestDate.toUTCString());
     }
 
-    return this.http.post<Post>(`${this.baseURL}${this.postsEndpoint}`, post, cudOptions)
-      .pipe(
-        catchError(this.handleError<Post>(post))
-      );
+    const serverResponse$ = this.http.get(
+      `${this.baseURL}${this.postsEndpoint}?forumId=${forumId}`,
+      {headers, observe: 'response'}
+    );
+
+    const updatedPosts$ = serverResponse$.pipe(
+      catchError(() => EMPTY),
+      filter(response => response.status === 200),
+      tap(response => {
+        const newPosts = response.body as Post[];
+        const lastModified = new Date(response.headers.get('Last-Modified'));
+        relevantPosts.forEach(post => this.cache.delete(post.id));
+        newPosts.forEach(post => this.cache.set(post.id, [post, lastModified]));
+      }),
+      pluck('body'),
+    );
+
+    return concat(cachedPosts$, updatedPosts$).pipe(defaultIfEmpty([]));
   }
 
   /**
-   * Handle an error thrown by an observable and instead return an Observable
-   * of some default value.
+   * POSTs a new Post to be created on the server and then returns the newly
+   * created resource from the body of a 201 response.
    *
-   * TODO - introduce some logging service and log the error
+   * Returns null on a non-201 response.
    *
-   * @param defaultValue to return
+   * Throws an error if the newPost parameter has an id as ids can only be
+   * issued by the server
+   *
+   * If a newly created post is returned it is stored in the cache.
+   *
+   * @param newPost The new forum to be created on the server
    */
-  private handleError<T>(defaultValue?: T) {
-    return (error: any): Observable<T> => of(defaultValue as T);
+  addPost(newPost: Post): Observable<Post> {
+    if (newPost.id) {
+      throw new Error('Cannot add a forum that contains an ID: ID\'s are added by the server');
+    }
+
+    const postRequest$ = this.http.post<Post>(
+      `${environment.backendHost}/posts`,
+      newPost,
+      cudOptions);
+
+    // successful 201 responses result in returning the newly created form
+    // other responses elicit a return of null
+    return postRequest$.pipe(
+      map(response => {
+        if (response.status === 201) {
+          // on successful response, update the cache and return the value
+          this.cache.set(response.body.id, [response.body, new Date(0)]);
+          return response.body;
+        } else {
+          return null;
+        }
+      }),
+      catchError(() => of(null)));
   }
 }
